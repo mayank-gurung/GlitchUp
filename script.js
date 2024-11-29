0400a8e07220c6d52f0a2fa1f9cf4b3e9f1c4886
MAYANK GURUNG <mayankgurung8770@gmail.com>
2024-11-29 15:49:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Side Bar Menu Icon -----
+
+let menuIcon = document.querySelector(".menu-icon");
+let sideBar = document.querySelector(".sidebar");
+let container = document.querySelector(".container");
+
+menuIcon.onclick = function() {
+    sideBar.classList.toggle("small-sidebar");
+    container.classList.toggle("large-container");
+}
